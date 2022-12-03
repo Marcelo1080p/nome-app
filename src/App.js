abe5ccf9e5cc2a87855581e96a4893d6bb946ad8
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-03 16:11:05 -0300

    feita adição de novas frases

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React, { useState, useEffect } from "react";
+import './App.css'
 
 function App() {
   const[nameInput, setNameInput] = useState('')
-  const[nameReverse, setNameReverse] = useState("")
-  useEffect(() =>{
-    reverter()
-  },)
+  const[nameReverse, setNameReverse] = useState('')
+  const[satiras, setSatiras] = useState("");
+  const msg = [`Sério... logo ${nameReverse}? Que nome Feio kkkkkk`, `${nameReverse.toUpperCase()} Triste esse nome não?`,`Pensando bem... até que da pra mudar o nome do RG de ${nameInput}, para ${nameReverse.toUpperCase()}?`, `Que dó, logo ${nameReverse.toUpperCase()} KKKKKKKKKKKKKKK. O destino certamente não gosta de você`, `${nameReverse.toUpperCase()} até que é bem fofo, não acha? kkkkkk, serio mesmo, sem Ironia :)`]
+  
+
+  
+
+  function returnSatiras() {
+    const sa = Math.floor(Math.random() * msg.length)
+    setSatiras(msg[sa])
+  }
+  
   function reverter(){
-    setNameReverse(nameInput.split('').reverse().join('').toUpperCase())
+    setNameReverse(nameInput.split('').reverse().join(''))
+    document.querySelector('input[name="limpar"]').value = ""
+    returnSatiras()
   }  
 
   return (
     <div className="App">
+      <h2>Descubra como fica o seu nome de trás para frente</h2>
       <label>Infome o seu nome: </label>
-      <input onChange={e => setNameInput(e.target.value)}/>
+      <input name="limpar" onChange={e => setNameInput(e.target.value)}/>
       <br/>
       <br/>
       <button onClick={reverter}>Reverter Nome</button>
       <br/>
       <br/>
-      <p>Seu nome: <span>{nameInput.toUpperCase()}</span></p>
-      <p>Seu nome de trás para frente: {nameReverse}</p>
+      <p>Seu nome: <span className="nomeReal">{nameInput}</span></p>
+      <p>Resultado: <span>{nameReverse.toUpperCase()}</span></p>
+      <p>{satiras}</p>
     </div>
   );
 
--- test.js
+++ test.js
@@ -1,3 +1,3 @@
-const nome = 'Marcelo'
-
-console.log(nome.split('').reverse().join(''))
+const satiras = ["Que nome Feio kkkkkk", "Triste esse nome não?", "Sério que você usaria esse nome?"];
+const satirasRandom = Math.floor(Math.random() * satiras.length);
+console.log(satiras[satirasRandom]);
