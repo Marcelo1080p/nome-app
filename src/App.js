8719d67f3aeaeb0397762aa2991dc4d7ec7d67e1
Marcelo1080p <juniooor.2015@gmail.com>
2022-12-03 17:20:32 -0300

    Começando a adiconar estilo

--- src/App.js
+++ src/App.js
@@ -1,41 +1,57 @@
 import React, { useState, useEffect } from "react";
-import './App.css'
+import "./App.css";
 
 function App() {
-  const[nameInput, setNameInput] = useState('')
-  const[nameReverse, setNameReverse] = useState('')
-  const[satiras, setSatiras] = useState("");
-  const msg = [`Sério... logo ${nameReverse}? Que nome Feio kkkkkk`, `${nameReverse.toUpperCase()} Triste esse nome não?`,`Pensando bem... até que da pra mudar o nome do RG de ${nameInput}, para ${nameReverse.toUpperCase()}?`, `Que dó, logo ${nameReverse.toUpperCase()} KKKKKKKKKKKKKKK. O destino certamente não gosta de você`, `${nameReverse.toUpperCase()} até que é bem fofo, não acha? kkkkkk, serio mesmo, sem Ironia :)`]
-  
-
-  
+  const [nameInput, setNameInput] = useState("");
+  const [nameReverse, setNameReverse] = useState("");
+  const [satiras, setSatiras] = useState("");
+  const msg = [
+    `Sério... logo ${nameReverse}? Que nome Feio kkkkkk`,
+    `${nameReverse.toUpperCase()} Triste esse nome não?`,
+    `Pensando bem... até que da pra mudar o nome do RG de ${nameInput}, para ${nameReverse.toUpperCase()}?`,
+    `Que dó, logo ${nameReverse.toUpperCase()} KKKKKKKKKKKKKKK. O destino certamente não gosta de você`,
+    `${nameReverse.toUpperCase()} até que é bem fofo, não acha? kkkkkk, serio mesmo, sem Ironia :)`,
+  ];
 
   function returnSatiras() {
-    const sa = Math.floor(Math.random() * msg.length)
-    setSatiras(msg[sa])
+    const sa = Math.floor(Math.random() * msg.length);
+    setSatiras(msg[sa]);
   }
-  
-  function reverter(){
-    setNameReverse(nameInput.split('').reverse().join(''))
-    document.querySelector('input[name="limpar"]').value = ""
-    returnSatiras()
-  }  
+
+  function reverter() {
+    setNameReverse(nameInput.split("").reverse().join(""));
+    document.querySelector('input[name="limpar"]').value = "";
+    returnSatiras();
+  }
 
   return (
     <div className="App">
-      <h2>Descubra como fica o seu nome de trás para frente</h2>
-      <label>Infome o seu nome: </label>
-      <input name="limpar" onChange={e => setNameInput(e.target.value)}/>
-      <br/>
-      <br/>
-      <button onClick={reverter}>Reverter Nome</button>
-      <br/>
-      <br/>
-      <p>Seu nome: <span className="nomeReal">{nameInput}</span></p>
-      <p>Resultado: <span>{nameReverse.toUpperCase()}</span></p>
-      <p>{satiras}</p>
+      <div className="container">
+        <h2>Descubra como fica o seu nome de trás para frente</h2>
+        <section>
+          <div className="entrada-dados">
+            <label>Infome o seu nome: </label>
+            <input
+              name="limpar"
+              required
+              onChange={(e) => setNameInput(e.target.value)}
+            />
+          </div>
+          <div className="container-button">
+            <button onClick={reverter}>Reverter Nome</button>
+          </div>
+          <div className="resposta-dados">
+            <p>
+              Seu nome: <span className="nomeReal">{nameInput}</span>
+            </p>
+            <p>
+              Resultado: <span>{nameReverse.toUpperCase()}</span>
+            </p>
+            <p>{satiras}</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
-
 }
 export default App;
--- test.js
+++
@@ -1,3 +0,0 @@
-const satiras = ["Que nome Feio kkkkkk", "Triste esse nome não?", "Sério que você usaria esse nome?"];
-const satirasRandom = Math.floor(Math.random() * satiras.length);
-console.log(satiras[satirasRandom]);
